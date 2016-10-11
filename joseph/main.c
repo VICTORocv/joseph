de0ffebfd70d6a3a365c31a1d0461aa04627f086
VICTORocv <gwqocv@icloud.con>
2016-10-11 16:01:46 +0800

    Initial Commit

---
+++ joseph/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  joseph
+//
+//  Created by 郭悟清 on 2016/10/11.
+//  Copyright © 2016年 郭悟清. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
